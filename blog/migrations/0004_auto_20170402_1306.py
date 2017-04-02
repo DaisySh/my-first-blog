96a2b87244b2a2429bfe9f920b21b0aa1c09710c
Dasara <shullani.dasara@gmail.com>
2017-04-02 14:00:36 +0200

    Modica template per vedere i post dal database.

---
+++ blog/migrations/0004_auto_20170402_1306.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_auto_20170402_1259'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='post',
+            old_name='publish_date',
+            new_name='published_date',
+        ),
+    ]
